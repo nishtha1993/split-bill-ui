245b2171bcff515b9b31926a165c0c3642b986a8
Parashara <parasharar@gmail.com>
2024-03-27 13:25:26 +0800

    testing calling the deployed api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [healthStatus, setHealthStatus] = useState('');
+
+  useEffect(() => {
+    fetchHealth();
+  }, []);
+
+  const fetchHealth = async () => {
+    try {
+      // const url = "http://127.0.0.1:8080" // for local testing
+      const url = "http://split-bill-ms-env.eba-2ps6wmpc.us-east-1.elasticbeanstalk.com" // for testing with the deployed cloud app
+      const response = await fetch(url + '/admin/health');
+      if (!response.ok) {
+        throw new Error('Failed to fetch');
+      }
+      const data = await response.text();
+      setHealthStatus(data);
+    } catch (error) {
+      console.error('Error fetching health status:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Health Status Response: {healthStatus}
         </p>
         <a
           className="App-link"
@@ -15,7 +37,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is a test to see if the split ui works!
+          Learn React
         </a>
       </header>
     </div>
